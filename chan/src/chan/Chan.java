c92b6580af0a0e0ed872ac4b4cbf8cde9f51f378
satthudatinh <quangtu104@gmail.com>
2021-10-21 11:44:06 +0700

    12
    12

---
+++ chan/src/chan/Chan.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Chan {
+      
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        int d;
+        
+        System.out.println("nhap a,b,c,d:");
+        Scanner s = new Scanner(System.in);
+        a =s.nextInt();
+        b =s.nextInt();
+        c =s.nextInt();
+        d =s.nextInt();
+        
+       
+        
+                
+             
+        
+        
+        
+        
+      
+      
+      
+      
+    
+    }
+}
